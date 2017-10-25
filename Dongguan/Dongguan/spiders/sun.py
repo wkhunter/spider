63e7ab36a396d9c924d47fad6a1731fcb64b6aae
xxbarca <xxbarca@163.com>
2017-10-25 23:09:34 +0800

    dongguan

---
+++ Dongguan/Dongguan/spiders/sun.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from Dongguan.items import DongguanItem
+
+class SunSpider(CrawlSpider):
+    name = 'sun'
+    allowed_domains = ['wz.sun0769.com']
+    start_urls = ['http://wz.sun0769.com/index.php/question/questionType?type=4&page=0']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'type=4&page=\d+')),
+        Rule(LinkExtractor(allow=r'/html/question/\d+/\d+.shtml'), callback = 'parse_item', follow = False)
+    )
+
+    def parse_item(self, response):
+        item = DongguanItem()
+        item['title'] = response.xpath('//div[@class="pagecenter p3"]//strong/text()').extract()[0]
+        item['number'] = item['title'].split(' ')[-1].split(':')[-1]
+        item['content'] = response.xpath('//div[@class="c1 text14_2"]/text()').extract()[0]
+        item['url'] = response.url
+
+        yield item
