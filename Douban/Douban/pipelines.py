c6b2435d81e7fa6982200dffd6fc75a9543b791a
xxbarca <xxbarca@163.com>
2017-11-07 10:27:51 +0800

    修改管道文件

--- Douban/Douban/pipelines.py
+++ Douban/Douban/pipelines.py
@@ -33,13 +33,18 @@ class DoubanPipeline(object):
 class DoubanJsonPipeline(object):
     def __init__(self):
         self.f = open('movie.json', 'w')
+        self.list = []
 
     def process_item(self, item, spider):
-        content = json.dumps(dict(item), ensure_ascii = False) + ",\n"
-        self.f.write(content)
+        # content = json.dumps(dict(item), ensure_ascii = False) + ",\n"
+        # self.f.write(content)
+        item = dict(item)
+        self.list.append(item)
         return item
 
     def close_spider(self, spider):
+        content = json.dumps(self.list, ensure_ascii = False, indent = 1)
+        self.f.write(content)
         self.f.close()
 
 
