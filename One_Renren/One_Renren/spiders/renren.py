843c40298cf43565abea38d0eb2f29d347543edc
xxbarca <xxbarca@163.com>
2017-11-09 21:28:08 +0800

    人人网模拟登录

---
+++ One_Renren/One_Renren/spiders/renren.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RenrenSpider(scrapy.Spider):
+    name = 'renren'
+    allowed_domains = ['http://zhibo.renren.com']
+    start_urls = ['http://http://zhibo.renren.com/']
+
+    def start_requests(self):
+    	url = 'http://www.renren.com/PLogin.do'
+    	yield scrapy.FormRequest(
+    			url = url,
+    			formdata = {
+    				'email': '18455086673',
+    				'password': 'admin123'
+    			},
+    			callback = self.parse_page
+    		)
+    def parse_page(self, response):
+    	with open('renren.html', 'wb') as f:
+    		f.write(response.body)
