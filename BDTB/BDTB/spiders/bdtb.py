b34c3e6d4656b6f7952e3aeee967a89b087c8192
xxbarca <xxbarca@163.com>
2017-12-01 19:46:17 +0800

    百度贴吧

---
+++ BDTB/BDTB/spiders/bdtb.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BdtbSpider(scrapy.Spider):
+    name = 'bdtb'
+    allowed_domains = ['https://tieba.baidu.com/']
+    start_urls = ['https://tieba.baidu.com/f?kw=巴塞罗那&fr=index']
+
+    def parse(self, response):
+        with open('response.html', 'w') as f:
+        	f.write(response.body)
