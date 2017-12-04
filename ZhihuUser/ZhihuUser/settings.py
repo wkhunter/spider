e4f4207c27e672e657e53874b673b893bc46a2f8
xxbarca <xxbarca@163.com>
2017-12-04 08:56:15 +0800

    知乎用户

--- ZhihuUser/ZhihuUser/settings.py
+++ ZhihuUser/ZhihuUser/settings.py
@@ -28,12 +28,12 @@ HTTPERROR_ALLOWED_CODES = [301, 302, 401]
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -73,9 +73,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'ZhihuUser.pipelines.ZhihuuserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'ZhihuUser.pipelines.ZhihuuserPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
