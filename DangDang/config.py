fd91c5ca48df02e1494faf91f3e673997170adb7
xxbarca <xxbarca@163.com>
2017-12-06 14:41:43 +0800

    当当图书

--- DangDang/config.py
+++ DangDang/config.py
@@ -1,3 +1,8 @@
 HEADERS = {
-	'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+	'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+	# 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+	# 'Accept-Encoding':'gzip, deflate',
+	# 'Cache-Control':'max-age=0',
+	# 'Host':'product.dangdang.com',
+	# 'Connection':'keep-alive',
 }
--- DangDang/spider.py
+++ DangDang/spider.py
@@ -4,6 +4,11 @@ from config import *
 from requests.exceptions import RequestException
 import re
 import json
+from multiprocessing import Pool
+import socket
+socket.setdefaulttimeout(3)
+from threading import Thread
+
 # 取种类列表
 def get_cate_text(url):
     try:
@@ -16,8 +21,6 @@ def get_cate_text(url):
 
 # 解析种类列表
 def get_cate_list(html):
-    with open('body.html', 'w') as f:
-        f.write(html)
     pattern = re.compile('<dt>.*?<a.*?href="(.*?)".*?>(.*?)</a>.*?</dt>', re.S)
     items = re.findall(pattern, html)
     cate_urls = set()
@@ -32,81 +35,56 @@ def get_book_text(url):
         if response.status_code == 200: 
             return response.text
         return None
-    except RequestException:
-        return None
+    except requests.exceptions.ConnectTimeout:
+        NETWORK_STATUS = False
+    except requests.exceptions.Timeout:
+        REQUEST_TIMEOUT = True
 
 
 # 解析图书列表
-def get_book_list(html):
-    pattern = re.compile('<li.*?ddt-pit.*?<a.*?class="pic".*?name="itemlist-picture".*?href="(.*?)".*?>.*?</a>.*?</li>', re.S)
-    urls = re.findall(pattern, html)
-    for url in urls:
-        html = get_each_book(url)
-        parse_each_book(html)
+def parse_book_list(html):
+    itemP = re.compile('<li.*?ddt-pit=.*?class=.*?id=.*?<a.*?title="(.*?)".*?ddclick.*?class.*?name.*?dd_name.*?href="(.*?)".*?>.*?<span.*?class.*?>&yen;(.*?)</span>.*?<a.*?href.*?dd_name.*?title=.*?>(.*?)</a>.*?</a>.*?</li>', re.S)
+    items = re.findall(itemP, html)
+    for item in items:
+        each = {}
+        each['title'] = item[0].strip()
+        each['link'] = item[1]
+        each['money'] = item[2] + '元'
+        each['author'] = item[3]
+        yield each
 
-# 取书详情页
-def get_each_book(url):
-    try:
-        response = requests.get(url, headers = HEADERS)
-        if response.status_code == 200:
-            return response.text
-        return None
-    except RequestException:
-        return None
+    # 下一页
+    nextP = re.compile('<li.*?class="next"><a.*?href="(.*?)".*?title.*?>.*?</a></li>', re.S)
+    next_url = re.findall(nextP, html)
+    if next_url:
+        next_url = 'http://category.dangdang.com/' + next_url[0]
+        html = get_book_text(next_url)
+        items = parse_book_list(html)
+        for item in items:
+            print('正在写入' + item['title'])
+            write_to_file(item)
+     
+# 写入文件
+def write_to_file(item):
+    with open('book.txt', 'a') as f:
+        f.write(json.dumps(item, ensure_ascii = False) + ',\n')
 
-# 解析书详情页
-def parse_each_book(html):
-    titlePat = re.compile('<h1.*?title="(.*?)".*?>.*?<img.*?>.*?</h1>', re.S)
-    title = re.findall(titlePat, html)
-    print(title[0].strip()) 
-
-
-
-
-def main():
-    url = 'http://book.dangdang.com/01.54.htm'
+def main(url):
+    
     # 取种类文本
     html = get_cate_text(url)
     # 取种类列表
     cate_urls = get_cate_list(html)
     for cate_url in cate_urls:
         html = get_book_text(cate_url)
-        get_book_list(html)
-
+        items = parse_book_list(html)
+        for item in items:
+            print('正在写入' + item['title'])
+            write_to_file(item)   
 
 if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    url = 'http://book.dangdang.com/01.54.htm'
+    main(url)
 
 
 
