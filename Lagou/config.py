c7842a4e571e8b9b500d28542cf7f31553818507
xxbarca <xxbarca@163.com>
2017-12-07 13:30:31 +0800

    拉钩

--- Lagou/config.py
+++ Lagou/config.py
@@ -1,6 +1,9 @@
 HEADERS = {
 	'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
 	'Referer':'https://www.lagou.com/jobs/list_Python?px=default&city=%E5%8C%97%E4%BA%AC',
+	'Origin':'https://www.lagou.com',
+	'Host':'www.lagou.com',
+	'Cookie':'user_trace_token=20171018150757-11d7072b-b3d3-11e7-9595-5254005c3644; LGUID=20171018150757-11d70ccb-b3d3-11e7-9595-5254005c3644; Hm_lvt_9d483e9e48ba1faa0dfceaf6333de846=1510029000,1510044572,1510069860,1510103314; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=5; index_location_city=%E5%85%A8%E5%9B%BD; JSESSIONID=ABAAABAACBHABBIE7CD5943FF544D0FEE9479A3B6501DD3; TG-TRACK-CODE=index_navigation; _gat=1; _gid=GA1.2.273848703.1512557694; _ga=GA1.2.589815693.1508310478; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1512206891,1512220618,1512557694,1512610017; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1512616430; LGSID=20171207101355-472ffbae-daf4-11e7-9c4b-5254005c3644; LGRID=20171207111349-a54c84d8-dafc-11e7-8804-525400f775ce; SEARCH_ID=3f98f136bc1d4fb98886a9f6fdebfff1'
 	# 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
 	# 'Accept-Encoding':'gzip, deflate',
 	# 'Cache-Control':'max-age=0',
--- Lagou/spider.py
+++ Lagou/spider.py
@@ -1,79 +1,79 @@
 import requests
 import xlwt
 from config import HEADERS
+import time
 
-
-def get_job_list():
+def get_one_page():
 	data = {
 		'first': 'true',
 		'pn': '1',
 		'kd': 'Python'
 	}
-	res = requests.post('https://www.lagou.com/jobs/positionAjax.json?px=default&city=%E5%8C%97%E4%BA%AC&needAddtionalResult=false&isSchoolJob=0', 
+
+	res = requests.post('https://www.lagou.com/jobs/positionAjax.json?px=default&city=%E4%B8%8A%E6%B5%B7&needAddtionalResult=false&isSchoolJob=0',
 						data = data, headers = HEADERS)
 	result = res.json()
-	jobs = result['content']['positionResult']['result']
-	return jobs
+	return result
 
-
-if __name__ == '__main__':
-	jobs = get_job_list()
+def get_job_list(pageSize):
 	# 创建excel对象
 	excel = xlwt.Workbook()
 	# 创建单元格对象
 	sheet = excel.add_sheet('lagou', cell_overwrite_ok = True)
-	sheet.write(0, 0, 'positionName')
-	sheet.write(0, 1, 'salary')
-	sheet.write(0, 2, 'workYear')
-	sheet.write(0, 3, 'education')
-	sheet.write(0, 4, 'jobNature')
-	sheet.write(0, 5, 'city')
-	sheet.write(0, 6, 'companyShortName')
-	sheet.write(0, 7, 'district')
-	sheet.write(0, 8, 'positionLables')
-	sheet.write(0, 9, 'secondType')
-	sheet.write(0, 10, 'companyFullName')
 	n = 1
-	for job in jobs:
-		sheet.write(n, 0, job['positionName'])
-		sheet.write(n, 1, job['salary'])
-		sheet.write(n, 2, job['workYear'])
-		sheet.write(n, 3, job['education'])
-		sheet.write(n, 4, job['jobNature'])
-		sheet.write(n, 5, job['city'])
-		sheet.write(n, 6, job['companyShortName'])
-		sheet.write(n, 7, job['district'])
-		sheet.write(n, 8, job['positionLables'])
-		sheet.write(n, 9, job['secondType'])
-		sheet.write(n, 10, job['companyFullName'])
-		n += 1
+	for pn in [x+1 for x in range(pageSize)]:
+		if pn == 1:
+			is_first = 'true'
+		else:
+			is_first = 'false'
 
-
-
-
+		data = {
+			'first': is_first,
+			'pn': str(pn),
+			'kd': 'Python',
+		}
+		res = requests.post('https://www.lagou.com/jobs/positionAjax.json?px=default&city=%E4%B8%8A%E6%B5%B7&needAddtionalResult=false&isSchoolJob=0',
+						data = data, headers = HEADERS)
+		time.sleep(2)
+		result = res.json()
+		jobs = result.get('content').get('positionResult').get('result')
+		sheet.write(0, 0, 'positionName')
+		sheet.write(0, 1, 'salary')
+		sheet.write(0, 2, 'workYear')
+		sheet.write(0, 3, 'education')
+		sheet.write(0, 4, 'jobNature')
+		sheet.write(0, 5, 'city')
+		sheet.write(0, 6, 'companyShortName')
+		sheet.write(0, 7, 'district')
+		sheet.write(0, 8, 'positionLables')
+		sheet.write(0, 9, 'secondType')
+		sheet.write(0, 10, 'companyFullName')
+		
+		for job in jobs:
+			sheet.write(n, 0, job['positionName'])
+			sheet.write(n, 1, job['salary'])
+			sheet.write(n, 2, job['workYear'])
+			sheet.write(n, 3, job['education'])
+			sheet.write(n, 4, job['jobNature'])
+			sheet.write(n, 5, job['city'])
+			sheet.write(n, 6, job['companyShortName'])
+			sheet.write(n, 7, job['district'])
+			sheet.write(n, 8, job['positionLables'])
+			sheet.write(n, 9, job['secondType'])
+			sheet.write(n, 10, job['companyFullName'])
+			n += 1
 	excel.save('lagou.xlsx')
 
 
+def main():
+	result = get_one_page()
+	pageSize = result['content']['pageSize']
+	time.sleep(2)	
+	get_job_list(pageSize)
 
 
+if __name__ == '__main__':
+	main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
 
