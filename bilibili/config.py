c9314a890aafc420fa3d0a9d1c14cf2a0aee6756
李 杨 <xxbarca@163.com>
2018-01-04 22:44:16 +0800

    bilibili用户

---
+++ bilibili/config.py
@@ -0,0 +1,25 @@
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+#     'X-Requested-With': 'XMLHttpRequest',
+#     'Referer': 'http://space.bilibili.com/45388',
+#     'Origin': 'http://space.bilibili.com',
+#     'Host': 'space.bilibili.com',
+#     'AlexaToolbar-ALX_NS_PH': 'AlexaToolbar/alx-4.0',
+#     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4',
+#     'Accept': 'application/json, text/javascript, */*; q=0.01',
+# }
+
+proxies = {
+    'http': 'http://61.155.164.108:3128',
+    'http': 'http://116.199.115.79:80',
+    'http': 'http://42.245.252.35:80',
+    'http': 'http://106.14.51.145:8118',
+    'http': 'http://116.199.115.78:80',
+    'http': 'http://123.147.165.143:8080',
+    'http': 'http://58.62.86.216:9999',
+    'http': 'http://202.201.3.121:3128',
+    'http': 'http://119.29.201.134:808',
+    'http': 'http://61.155.164.112:3128',
+    'http': 'http://123.57.76.102:80',
+    'http': 'http://116.199.115.78:80',
+}
---
+++ bilibili/spider.py
@@ -0,0 +1,119 @@
+import requests
+import random
+from config import *
+import datetime
+import time
+import json
+
+def datetime_to_timestamp_in_milliseconds(d):
+	def current_milli_time(): 
+		return int(round(time.time() * 1000))
+	return current_milli_time()
+
+def get_userAgent(uafile):
+	uas = []
+	with open(uafile, 'rb') as f:
+		for ua in f.readlines():
+			if ua:
+				uas.append(ua.decode('utf-8').strip())
+	random.shuffle(uas)
+	return uas
+
+uas = get_userAgent('user_agents.txt')
+
+def getUids():
+	for m in range(99, 101):
+		uids = []
+		for i in range(m * 100, (m + 1) * 100):
+			uids.append(i)
+	return uids
+# 粉丝
+def getFollowers(vmid):
+	pn = 1
+	ua = random.choice(uas)
+	headers = {
+		'User-Agent': ua,
+		'Referer': 'https://space.bilibili.com/' + str(vmid)
+	}
+	
+	while 1:
+		url = 'https://api.bilibili.com/x/relation/followers?vmid=' + str(vmid) + '&pn=' + str(pn) + '&ps=20'
+		jscontent = requests.get(url, headers = headers, proxies = proxies).text
+		dicData = json.loads(jscontent)
+		if dicData.get('code') == 0:
+			datalist = dicData.get('data').get('list')
+			for item in datalist:
+				mid = item.get('mid')
+				info = personInfo(mid)	
+			pn += 1				
+		else:
+			break
+
+# 关注
+
+
+# 个人信息
+def personInfo(uid):
+	params = {
+		'mid': str(uid)
+	}
+	ua = random.choice(uas)
+	headers = {
+		'User-Agent': ua,
+		'Referer': 'https://space.bilibili.com/' + str(uid)
+	}
+	jscontent = requests.session().post('http://space.bilibili.com/ajax/member/GetInfo', headers = headers, data = params, proxies = proxies).text
+	
+	try:
+		dicData = json.loads(jscontent)
+		status = dicData['status'] if 'status' in dicData.keys() else False
+		if status:
+			if 'data' in dicData.keys():
+				data = dicData.get('data')
+				info = {
+					'mid': data.get('mid'),
+					'name': data.get('name'),
+					'approve': data.get('approve'),
+					'sex': data.get('sex'),
+					'rank': data.get('rank'),
+					'face': data.get('face'),
+					'regtime': data.get('regtime'),
+					'place': data.get('place'),
+					'birthday': data.get('birthday'),
+					'sign': data.get('sign'),
+					'level': data.get('level_info').get('current_level'),
+					'image': data.get('nameplate').get('image'),
+					'exp': data.get('level_info').get('current_exp')
+				}
+				write_to_file(str(info))
+				print('正在下载-->', info.get('name'))
+				return info
+		else:
+			print('数据错误')
+
+	except ValueError:
+		pass
+
+def write_to_file(item):
+	with open('info.txt', 'a') as f:
+		f.write(item + '\n')
+		f.close()
+
+if __name__ == '__main__':
+	info = personInfo(85486475)
+	getFollowers(info.get('mid'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
